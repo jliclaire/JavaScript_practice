a1ac7cde9a8556043f20ed0e93fa5962d91ca0aa
jingli <jingli.claire@gmail.com>
2019-05-18 23:22:08 +1000

    add file generateRandomHexColor

---
+++ generateRandomColor.js
@@ -0,0 +1,14 @@
+const generateRandomColor = () =>{
+  const hexCodeArray = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+  let colorArray = [];
+
+  for(let i = 0; i < 6; i++){    
+    let randomIndex = Math.floor(Math.random() * 15);
+    console.log(randomIndex);
+    colorArray.push(hexCodeArray[randomIndex]);  
+  }
+
+  return String(colorArray.join(''));
+}
+
+console.log(generateRandomColor());
